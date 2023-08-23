fe2e6188cce17bc19dde0de6966b8da4c037a1e4
Dom-Owens-UoB <dom.owens@bristol.ac.uk>
2023-08-23 13:36:55 +0100

    dash update

--- dashboard.py
+++ dashboard.py
@@ -1,12 +1,19 @@
 from dash import Dash, html, dcc, dash_table, callback, Input, Output
 import pairs
 
+
+
+
+
+
+## app
 app = Dash(__name__)
+
+fig, tab = None, None
+df = pairs.get_df()
 
 app.layout = html.Div([
     html.H1("Pairs Trading"),
-    dash_table.DataTable(id="portfolio-table", page_size=10),
-    dcc.Graph(id="portfolio-graph"),
     html.Label("Lookback Period:"),
     dcc.Slider(
         id="lookback-slider",
@@ -25,18 +32,22 @@ app.layout = html.Div([
         id="stock-dropdown2",
         options=[{"label": stock_name2, "value": stock_name2} for stock_name2 in ["AAPL", "MSFT", "TSLA"]],
         value="MSFT"
-    )
+    ),
+    #dash_table.DataTable(id="portfolio-table", page_size=10),
+    dcc.Graph(id="portfolio-graph", figure = fig)
 ])
 
 @app.callback(
     Output("portfolio-graph", "figure"),
-    Input("lookback-slider", "value"),
+    Output("portfolio-tab", "dash_table"),
     Input("stock-dropdown1", "value"),
-    Input("stock-dropdown2", "value")
+    Input("stock-dropdown2", "value"),
+    Input("lookback-slider", "value")
 )
 def update_graph(stock1, stock2, lookback_period):
     # Apply the pairs trading strategy
-    trade_out = pairs.trade(stock1, stock2, capital=1.0,
+
+    trade_out = pairs.trade(df[stock1], df[stock2], capital=1.0,
                             lookback_period=lookback_period, entry_threshold=1.0, exit_threshold=0.2)
 
     # Create a plot of portfolio value
@@ -53,6 +64,8 @@ def update_graph(stock1, stock2, lookback_period):
 
 
     return figure, table
+
+#fig, tab = update_graph(stock1, stock2, lookback_period)
 
 
 if __name__ == "__main__":
